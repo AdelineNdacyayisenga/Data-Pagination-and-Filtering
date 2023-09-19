914952c61728226e9b4ebb6b3b8482a4a1a6a409
Adeline Ndacyayisenga <ndacya.adeline@gmail.com>
2023-09-18 20:27:26 -0700

    added the addPagination function and an event listener for the buttons

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -7,8 +7,9 @@ FSJS Project 2 - Data Pagination and Filtering
 const itemsPerPage = 9;
 
 /*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
+@param list - an array of items to display on the pages
+@param page - represents the requested page number
 */
 
 function showPage (list, page) {
@@ -40,13 +41,60 @@ function showPage (list, page) {
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+Creates and inserts/appends the elements needed for the pagination buttons
+@param list - an array of items to display on the pages
 */
-
-function addPagination () {
+const linkList = document.querySelector('.link-list'); //the element we will be adding to the pagination buttons
+function addPagination (list) {
+   const buttons = Math.ceil(list.length/itemsPerPage);
    
+   linkList.innerHTML = ''; //removes any existing pagination buttons that might have been displayed previously
+
+   for (let i = 1; i <= buttons; i ++) {
+      const button = `
+      <li>
+         <button type="button">${i}</button>
+      </li>
+      `;
+      linkList.insertAdjacentHTML('beforeend', button);
+      
+   }
+
+   linkList.querySelector('button').classList.add('active');
+
 }
 
-// Call functions
+/*
+Listens for clisks on the buttons and displays the corresponding items on the page
+@param e -is the current click event
+*/
+
+linkList.addEventListener('click', (e) => {
+
+   const event = e.target;
+   const activeButton = document.querySelector('.active');
+   const isClicked = event.closest('button');//makes sure user clicked the button
+
+   if(activeButton && isClicked) {
+      activeButton.className = ''; //remove active class from any other button
+   }
+   if(isClicked) {
+      isClicked.classList.add('active'); //add the active class to the clicked button
+      showPage(data, isClicked.innerHTML);
+   }
+
+   //Alternative Solution
+   
+   // if(event.tagName === 'BUTTON') { //i.e the element is clicked
+   //    //first active button
+   //    const activeButton = document.querySelector('.active');
+   //    activeButton.className = '';
+   //    event.className = 'active'; //adds active to the button that was clicked
+   //    showPage(data, event.textContent);
+   // }
+});
+
+// Calls the functions
+
 showPage(data, 1);
+addPagination(data);
